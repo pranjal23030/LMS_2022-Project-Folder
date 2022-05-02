755a097c8f66564f33654cca831033ef8381949d
Pranjal Khatiwada <pranjal.khatiwoda@gmail.com>
2022-05-02 10:53:04 +0545

    User Updated

--- update_user.php
+++ update_user.php
@@ -1,12 +1,13 @@
 <?php
     //Database connection
+    session_start();
+
     $connection = mysqli_connect("localhost","root","");
     $db = mysqli_select_db($connection,"lms_project folder");
 
-
-    //Query
-    $sql = "UPDATE user SET name ='$_POST[name]', contact ='$_POST[contact]', email ='$_POST[email]', address ='$_POST[address]'";
+    $sql = "UPDATE user SET name ='$_POST[name]', contact ='$_POST[contact]', email ='$_POST[email]', address ='$_POST[address]' WHERE id=".$_SESSION['id'];
     $run_sql = mysqli_query($connection,$sql);
+
     ?>
 <script type="text/javascript">
     alert ("Updated successfully!!");
